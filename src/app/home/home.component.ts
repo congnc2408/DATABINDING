8c8214136e23cbb0cbae1b38a98510db9c5f150c
Congncph16076 <congnc2408@gmail.com>
2023-08-21 01:16:14 +0700

    for-pipe-class

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,18 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   public name = 'Công';
   public age = 15;
+  public traiCay = [
+    {ten: 'Táo',gia: 12 ,haGia: true},
+    {ten: 'Nho',gia: 15,haGia: false},
+    {ten: 'Cam',gia: 30,haGia: true},
+    {ten: 'Quýt',gia: 15,haGia: false}
+
+  ]
 
 
+  public resetName():void {
+    this.name= '';
+
+  }
 
 }
